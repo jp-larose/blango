05674f23316a822508f4f85f9ec86afa08e0551e
Joël Larose <joel.larose@gmail.com>
2023-07-08 03:35:04 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,27 +1,61 @@
 from django.contrib.auth.models import User
-from django.utils.html import format_html
+from django.utils.html import format_html, format_html_join
 from django.template import Library
+
+from blog.models import Post
 
 register = Library()
 
 @register.filter
-def author_details(author, current_user=None):
-  if author is None or not isinstance(author, User):
-    return ""
+def author_details(author, current_user):
+    if not isinstance(author, User):
+        # return empty string as safe default
+        return ""
 
-  if author == current_user:
-    return format_html("<strong>me</strong>")
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html('{}{}{}', prefix, name, suffix)
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+  request = context["request"]
+  current_user = request.user
+  post = context["post"]
+  author = post.author
+
+  return author_details(author, current_user)
+
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
   
-  if author.first_name and author.last_name:
-    name = f"{author.first_name} {author.last_name}"
-  else:
-    name = author.username
+@register.simple_tag
+def col(extra_classes='', **kwargs):
+  kw = format_html_join(' ', '{}="{}"', kwargs.items())
+  return format_html('<div class="col {}" {}>', extra_classes, kw)
 
-  prefix = ""
-  suffix = ""
-  if author.email:
-    prefix = format_html('<a href="mailto:{}">', author.email)
-    suffix = format_html('</a>')
+@register.simple_tag
+def endcol():
+  return format_html('</div>')
 
-  return format_html('{}{}{}', prefix, name, suffix)
-
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(excluded_post):
+  posts = Post.objects.exclude(pk=excluded_post.pk)[:5]
+  return {"title": "Recent Posts", "posts": posts}
