5896dbb2231607805246d4e6b358624137b566a4
Joël Larose <joel.larose@gmail.com>
2024-01-23 18:03:58 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -64,6 +64,7 @@ class Dev(Configuration):
         'allauth.account',
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
+        'rest_framework.authtoken',
     ]
 
     MIDDLEWARE = [
@@ -222,6 +223,14 @@ class Dev(Configuration):
             "handlers": ["console"],
             "level": "DEBUG",
         },
+    }
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
     }
 
 class Prod(Dev):
